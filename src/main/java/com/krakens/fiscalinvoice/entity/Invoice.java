52827c9706c1a5ad7e778d156b6891a2978e00a9
Carlos <almendracarloshenrique@gmail.com>
2025-04-24 19:47:02 -0300

    added invoice entity class

---
+++ src/main/java/com/krakens/fiscalinvoice/entity/Invoice.java
@@ -0,0 +1,27 @@
+package com.krakens.fiscalinvoice.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String stateCode;
+    private double taxRate;
+    private double total;
+}
+
+
