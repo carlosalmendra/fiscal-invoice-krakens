baaa2d4a48ad1fc5137599b3ea61043ad31b027e
Carlos <almendracarloshenrique@gmail.com>
2025-04-23 21:12:28 -0300

    first commit

---
+++ src/main/java/com/krakens/fiscal_invoice/FiscalInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.krakens.fiscal_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakens/fiscal_invoice/FiscalInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakens.fiscal_invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
