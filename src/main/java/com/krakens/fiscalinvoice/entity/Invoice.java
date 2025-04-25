30a0df3ae62bdbee9e5e11f66373660dd903468d
Carlos <almendracarloshenrique@gmail.com>
2025-04-24 21:07:39 -0300

    added new constructor in invoice entity class

--- src/main/java/com/krakens/fiscalinvoice/entity/Invoice.java
+++ src/main/java/com/krakens/fiscalinvoice/entity/Invoice.java
@@ -1,5 +1,6 @@
 package com.krakens.fiscalinvoice.entity;
 
+import com.krakens.fiscalinvoice.dto.InvoiceDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,10 +17,17 @@ public class Invoice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String stateCode;
     private double taxRate;
     private double total;
+
+    public Invoice (InvoiceDTO invoiceDTO) {
+        this.id = invoiceDTO.getId();
+        this.stateCode = invoiceDTO.getStateCode();
+        this.taxRate = invoiceDTO.getTaxRate();
+        this.total = invoiceDTO.getTotal();
+    }
 }
 
 
